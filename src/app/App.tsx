c75be488ae71c01b598184dc045d429481c8154d
JuanmiR74 <juanmiroche@gmail.com>
2026-04-18 19:34:14 +0200

    Add files via upload

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,5 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { LoginPage } from '../pages/LoginPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
+import ProtectedRoute from '../components/auth/ProtectedRoute';
 import DashboardPage from '../pages/DashboardPage';
-import { ProtectedRoute } from '../components/auth/ProtectedRoute';
-
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <DashboardPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
-}
+export default function App() { return (<BrowserRouter><Routes><Route path="/login" element={<LoginPage />} /><Route path="/" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} /></Routes></BrowserRouter>); }
