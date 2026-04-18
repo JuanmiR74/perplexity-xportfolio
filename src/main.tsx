5dddd1ff9f79a3b335d62520945fc54c903770e6
JuanmiR74 <juanmiroche@gmail.com>
2026-04-18 18:58:01 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'sonner';
-import App from './app/App';
+import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import './styles.css';
 
