1273cc6fe88eb7aec36ed49c20286988c8895933
JuanmiR74 <juanmiroche@gmail.com>
2026-04-18 20:40:27 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path='/login' element={<Login />} />
         <Route
-          path="/"
+          path='/'
           element={
             <ProtectedRoute>
               <DashboardPage />
             </ProtectedRoute>
           }
         />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path='*' element={<Navigate to='/login' replace />} />
       </Routes>
     </BrowserRouter>
   );
