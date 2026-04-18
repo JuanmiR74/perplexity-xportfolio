09b900d49ff14f9a595698af4ec8eecb7bf9a286
JuanmiR74 <juanmiroche@gmail.com>
2026-04-18 19:50:08 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'sonner';
-import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
-import './styles.css';
-
+import App from './app/App';
 const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-          <Toaster richColors position="top-right" />
-        </BrowserRouter>
-      </AuthProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<React.StrictMode><QueryClientProvider client={queryClient}><App /><Toaster position="top-right" /></QueryClientProvider></React.StrictMode>);
