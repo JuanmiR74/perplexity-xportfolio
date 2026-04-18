2a76cf7b647dee62d0130c58db17bd924cf25b86
JuanmiR74 <juanmiroche@gmail.com>
2026-04-18 20:07:26 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from 'sonner';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
+import './index.css';
 
-const queryClient = new QueryClient();
+( window as any ).React = React;
+( window as any ).ReactDOM = ReactDOM;
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-        <Toaster position="top-right" />
-      </AuthProvider>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
