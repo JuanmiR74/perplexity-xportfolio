ec85a99aea7a33aef66ee9d9e21e601557f34d73
JuanmiR74 <juanmiroche@gmail.com>
2026-04-19 00:52:33 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import ProtectedRoute from './components/auth/ProtectedRoute';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route
-          path='/'
-          element={
-            <ProtectedRoute>
-              <DashboardPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path='*' element={<Navigate to='/login' replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/' element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+      <Route path='*' element={<Navigate to='/login' replace />} />
+    </Routes>
   );
 }
